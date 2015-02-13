6d385b59967438e884d6a480079ea3d364760778
Steven Sucy <sms6462@g.rit.edu>
2015-02-12 21:10:57 -0500

    Set up rough base classes and simple Seek implementation

---
+++ Assets/Scripts/character/Character.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// The base class for AI characters that are controlled by
+/// a NavMeshAgent component along with queued actions.
+/// </summary>
+public class Character : MonoBehaviour {
+
+	private readonly Queue<Action> actionQueue = new Queue<Action>();
+	private NavMeshAgent agent;
+	private GameObject following;
+
+	/// <summary>
+	/// Retrieves the NavMeshAgent component of the character
+	/// to handle controlling movement patterns
+	/// </summary>
+	/// <value>The agent of the character</value>
+	public NavMeshAgent Agent {
+		get { return agent; }
+	}
+
+	/// <summary>
+	/// Grabs the NavMeshAgent component on startup
+	/// </summary>
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		QueueAction(new Seek(new Vector3(4.38f, 0.61f, -5.72f)));
+	}
+
+	/// <summary>
+	/// Updates the character by applying queued actions
+	/// </summary>
+	void Update () {
+		actionQueue.Peek().Apply(this);
+	}
+
+	/// <summary>
+	/// Seeks towards a target's position
+	/// </summary>
+	/// <param name="target">The target to follow</param>
+	public void Follow(GameObject target) {
+		following = target;
+	}
+
+	/// <summary>
+	/// Queues a new action for the character to follow.
+	/// </summary>
+	/// <param name="action">Action to queue</param>
+	public void QueueAction(Action action) {
+		actionQueue.Enqueue(action);
+	}
+
+	/// <summary>
+	/// Forces the character to follow the given action, cancelling
+	/// all other actions.
+	/// </summary>
+	/// <param name="action">Action to force the character to follow</param>
+	public void ForceAction(Action action) {
+		actionQueue.Clear();
+		actionQueue.Enqueue(action);
+	}
+}
---
+++ Assets/Scripts/character/Robber.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Robber : Character {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
