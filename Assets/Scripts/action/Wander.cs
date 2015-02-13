508345daa9347d5e1cb5e0adc56354e8514ae593
Sean Brennan <esean17@yahoo.com>
2015-02-13 09:49:13 -0500

    Started the wander function

---
+++ Assets/Scripts/action/Wander.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wander : Action {
+
+	public int wanderRange = 10;
+	private Vector3 target;
+
+	/// <summary>
+	/// Creates a wander action that causes the target
+	/// to pick a random location within range and move there
+	/// </summary>
+	/// <param name="character">the character that will be wandering</param>
+	public Wander (Wanderer character) {
+		target = character.transform.position + new Vector3(Random.Range(-wanderRange, 0, wanderRange);
+	}
+
+	/// <summary>
+	/// Updates the target of the character, moving them
+	/// closer to the target position
+	/// </summary>
+	/// <param name="character">The character controlled by the action</param>
+	public void Apply (Wanderer character) {
+	
+	}
+}
---
+++ Assets/Scripts/character/Wanderer.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// The base class for AI characters that are controlled by
+/// a NavMeshAgent component along with queued actions.
+/// </summary>
+public class Wanderer : MonoBehaviour {
+	
+	private readonly Queue<Action> actionQueue = new Queue<Action>();
+	private NavMeshAgent agent;
+	
+	/// <summary>
+	/// Retrieves the NavMeshAgent component of the character
+	/// to handle controlling movement patterns
+	/// </summary>
+	/// <value>The agent of the character</value>
+	public NavMeshAgent Agent {
+		get { return agent; }
+	}
+	
+	/// <summary>
+	/// Grabs the NavMeshAgent component on startup
+	/// </summary>
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		QueueAction(new Wander(this));
+	}
+	
+	/// <summary>
+	/// Updates the character by applying queued actions
+	/// </summary>
+	void Update () {
+		actionQueue.Peek().Apply(this);
+		if (!agent.pathPending)
+		{
+			if (agent.remainingDistance <= agent.stoppingDistance)
+			{
+				if (!agent.hasPath || cagent.velocity.sqrMagnitude ==0f)
+				{
+					QueueAction(new Wander(this));
+				}
+			}
+		}
+	}
+	
+	/// <summary>
+	/// Queues a new action for the character to follow.
+	/// </summary>
+	/// <param name="action">Action to queue</param>
+	public void QueueAction(Action action) {
+		actionQueue.Enqueue(action);
+	}
+	
+	/// <summary>
+	/// Forces the character to follow the given action, cancelling
+	/// all other actions.
+	/// </summary>
+	/// <param name="action">Action to force the character to follow</param>
+	public void ForceAction(Action action) {
+		actionQueue.Clear();
+		actionQueue.Enqueue(action);
+	}
+}
