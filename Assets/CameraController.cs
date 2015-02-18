928b0ece75e04b3dda0bd5d5b37b4d27777a68fa
Steven Sucy <sms6462@g.rit.edu>
2015-02-18 09:41:27 -0500

    Fixed the camera

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -3,39 +3,43 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	Vector3 displacement;
-	Quaternion rotation;
-	Vector3 eulerAngle;
-	Vector3 forward;
-	Vector3 right;
+	public float TRANSLATE_SPEED = 0.25f;
+	public float ROTATE_SPEED = 0.2f;
+
+	private Vector3 eulerAngle;
 
 	// Use this for initialization
 	void Start ()
 	{
-		displacement = Vector3.zero;
-		rotation = Quaternion.identity;
-		eulerAngle = Quaternion.Euler(new Vector3(0.0f, 10.0f, 0.0f)).eulerAngles;
-		forward = Quaternion.Euler (-1.0f * gameObject.transform.rotation.eulerAngles) * gameObject.transform.forward;
-		right = gameObject.transform.right;
+		eulerAngle = Quaternion.Euler(new Vector3(0.0f, 10.0f * ROTATE_SPEED, 0.0f)).eulerAngles;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		displacement = Vector3.zero;
-		rotation = Quaternion.identity;
+		Vector3 displacement = Vector3.zero;
+		Quaternion rotation = Quaternion.identity;
 
-		if (Input.GetKeyDown (KeyCode.W)) {
+		Vector3 forward = transform.forward;
+		forward.Set(forward.x, 0, forward.z);
+		forward = forward.normalized * TRANSLATE_SPEED;
+
+		if (Input.GetKey (KeyCode.W)) {
 			displacement = forward;
-		} else if (Input.GetKeyDown (KeyCode.S)) {
-			displacement = -1.0f * forward;
-		} else if (Input.GetKeyDown (KeyCode.A)) {
-			displacement = -1.0f * right;
-		} else if (Input.GetKeyDown (KeyCode.D)) {
-			displacement = right;
-		} else if (Input.GetKeyDown (KeyCode.Q)) {
-			rotation = Quaternion.Euler(-1.0f * eulerAngle);
-		} else if (Input.GetKeyDown (KeyCode.E)) {
+		} 
+		if (Input.GetKey (KeyCode.S)) {
+			displacement = -forward;
+		} 
+		if (Input.GetKey (KeyCode.A)) {
+			displacement = -transform.right * TRANSLATE_SPEED;
+		} 
+		if (Input.GetKey (KeyCode.D)) {
+			displacement = transform.right * TRANSLATE_SPEED;
+		}
+		if (Input.GetKey (KeyCode.Q)) {
+			rotation = Quaternion.Euler(-eulerAngle);
+		} 
+		if (Input.GetKey (KeyCode.E)) {
 			rotation = Quaternion.Euler(eulerAngle);
 		}
 		
