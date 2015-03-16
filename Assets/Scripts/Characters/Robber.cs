ed0e4e24e4f147503de28dd4ea273e243777d1c0
Steven Sucy <sms6462@g.rit.edu>
2015-03-16 08:29:30 -0400

    Blocked out Robber methods

--- Assets/Scripts/Characters/Robber.cs
+++ Assets/Scripts/Characters/Robber.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class Robber : Character 
 {
+	// Target store to rob
+	private Building store;
+
+	// Whether or not the robber finished robbing something
+	private bool hasRobbed = false;
+
 	// Use this for initialization
 	override protected void Start () 
 	{
@@ -13,5 +19,223 @@ public class Robber : Character
 	override protected void Update () 
 	{
 	
+	}
+
+	public void MakeDecision() {
+		DecisionTree.Node node = dTree.Root;
+		bool inTree = true;
+		while (inTree)
+		{
+			bool result = true;
+			switch (node.Data)
+			{
+			case "Has Target":
+				result = HasTarget();
+				break;
+			case "IsCrowded":
+				result = IsCrowded();
+				break;
+			case "IsImpatient":
+				result = IsImpatient();
+				break;
+			case "IsReckless":
+				result = IsReckless();
+				break;
+			case "Rob":
+				result = Rob();
+				break;
+			case "ForgetTarget":
+				result = ForgetTarget();
+				break;
+			case "StalkTarget":
+				result = StalkTarget();
+				break;
+			case "HasRobbed":
+				result = HasRobbed();
+				break;
+			case "Run":
+				result = Run();
+				break;
+			case "IsChaotic":
+				result = IsChaotic();
+				break;
+			case "GetNearest":
+				result = GetNearest();
+				break;
+			case "Chance":
+				result = Chance();
+				break;
+			case "GetTarget":
+				result = GetTarget();
+				break;
+			case "Wander":
+				result = Wander();
+				break;
+				
+			default:
+				break;
+			}
+			if (node.IsLeaf()) inTree = false;
+			else
+			{
+				if (result) node = node.YesPtr;
+				else node = node.NoPtr;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Do I have a store in mind to rob?
+	/// </summary>
+	/// <returns><c>true</c> if this instance has target; otherwise, <c>false</c>.</returns>
+	private bool HasTarget() 
+	{
+		return store != null;
+	}
+
+	/// <summary>
+	/// Is there too many people around?
+	/// </summary>
+	/// <returns>True if crowded, false otherwise</returns>
+	private bool IsCrowded()
+	{
+		// TODO actually check for nearby cops or many nearby citizens
+		return false;
+	}
+
+	/// <summary>
+	/// Have I been waiting too long?
+	/// </summary>
+	/// <returns><c>true</c> if this instance is impatient; otherwise, <c>false</c>.</returns>
+	private bool IsImpatient()
+	{
+		// TODO implement individual characteristics
+		return false;
+	}
+
+	/// <summary>
+	/// Seems like a bad idea, should I do it anyway?
+	/// </summary>
+	/// <returns><c>true</c> if this instance is reckless; otherwise, <c>false</c>.</returns>
+	private bool IsReckless()
+	{
+		// TODO implement individual characteristics
+		return false;
+	}
+
+	/// <summary>
+	/// Rob the current target store
+	/// </summary>
+	private bool Rob()
+	{
+		// TODO rob the store
+		return false;
+	}
+
+	/// <summary>
+	/// Forgets the current target due to waiting for too long
+	/// </summary>
+	/// <returns>True</returns>
+	private bool ForgetTarget()
+	{
+		this.store = null;
+		return true;
+	}
+
+	/// <summary>
+	/// I want to rob here, but I'll wait around for a little.
+	/// </summary>
+	/// <returns>False</returns>
+	private bool StalkTarget()
+	{
+		// TODO add stalking behavior (either slow movement nearby or just idle)
+		return false;
+	}
+
+	/// <summary>
+	/// Did I rob something already
+	/// </summary>
+	/// <returns>True if robbed a store, false otherwise</returns>
+	private bool HasRobbed()
+	{
+		return hasRobbed;
+	}
+
+	/// <summary>
+	/// I got the goods, I should get away!
+	/// </summary>
+	private bool Run() 
+	{
+		// TODO make a getaway target for the robber
+		return false;
+	}
+
+	/// <summary>
+	/// Are there other robberies distacting attention away from here?
+	/// </summary>
+	/// <returns>True if scene is chaotic, false otherwise</returns>
+	private bool IsChaotic()
+	{
+		// TODO check for cops leaving to go to other robberies
+		return false;
+	}
+
+	/// <summary>
+	/// Gets the nearest store and makes it the robber's target
+	/// </summary>
+	/// <returns>true</returns>
+	private bool GetNearest()
+	{
+		GameObject closestStore = City.GetNearest (City.shops, gameObject);
+		GameObject closestBank = City.GetNearest (City.banks, gameObject);
+
+		// Get the closer of the two for the target
+		if ((closestBank.transform.position - transform.position).sqrMagnitude 
+		    < (closestStore.transform.position - transform.position).sqrMagnitude) 
+		{
+			store = closestBank.GetComponent<Building>();
+		}
+		else 
+		{
+			store = closestStore.GetComponent<Building>();
+		}
+
+		return true;
+	}
+
+	/// <summary>
+	/// Leave something up to chance
+	/// </summary>
+	private bool Chance()
+	{
+		return Random.value > 0.2;
+	}
+
+	/// <summary>
+	/// GGet a random store to rob from
+	/// </summary>
+	/// <returns><c>true</c>, if target was gotten, <c>false</c> otherwise.</returns>
+	private bool GetTarget()
+	{
+		// Get a random bank or shop
+		if (Random.value > 0.2)
+		{
+			store = City.GetRandom (City.shops).GetComponent<Building>();
+		}
+		else 
+		{
+			store = City.GetRandom (City.banks).GetComponent<Building>();
+		}
+
+		return true;
+	}
+
+	/// <summary>
+	/// Wander about the city
+	/// </summary>
+	private bool Wander() 
+	{
+		QueueAction(new Wander(4));
+		return true;
 	}
 }
