07341bcabe5000387162bd2f88fa7d8147068b64
Sean Brennan <esean17@yahoo.com>
2015-04-08 09:51:18 -0400

    Began work on the player controller.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float jumpHeight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float forwardMovement = Input.GetAxis("Vertical");
+		float horizontalMovement = Input.GetAxis("Horizontal");
+
+		float jump = Input.GetAxis("Jump");
+
+		Debug.Log("Vertical: " + forwardMovement + ", Horizontal: " + horizontalMovement + ", Jump: " + jump);
+
+		if( horizontalMovement > 0.5f )
+		{
+
+		}
+
+	}
+}
