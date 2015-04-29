3d349e0c1272d13362be2092f4377ae545bc5bcb
unknown <twb1193@GDD-21.main.ad.rit.edu>
2015-04-29 09:27:00 -0400

    Fixed a warning.

--- Assets/AI/Actions/RainGotAway.cs
+++ Assets/AI/Actions/RainGotAway.cs
@@ -10,7 +10,7 @@ public class RainGotAway : ActionBase
     public override ActionResult Execute()
     {
 		character.target = null;
-		(character as Robber).trigger.collider.active = false;
+		(character as Robber).trigger.collider.enabled = false;
         return ActionResult.SUCCESS;
     }
 }
