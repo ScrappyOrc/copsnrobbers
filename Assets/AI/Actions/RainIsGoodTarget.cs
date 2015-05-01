27bc427458f1b3dce554bcf97939347e05ae4e72
Steven Sucy <sms6462@g.rit.edu>
2015-05-01 08:03:31 -0400

    Changed target selection to prevent visiting the same place repeatedly

--- Assets/AI/Actions/RainIsGoodTarget.cs
+++ Assets/AI/Actions/RainIsGoodTarget.cs
@@ -10,9 +10,12 @@ public class RainIsGoodTarget : ActionBase
     public override ActionResult Execute()
     {
 		// check to see if the target character/building has a lot of money to rob
+		Debug.Log("Target: " + character.target);
+		Debug.Log("Money: " + character.target.cashRegister);
 		if( character.target.cashRegister > 250)
 			return ActionResult.SUCCESS;
 
+		character.target = null;
         return ActionResult.FAILURE;
     }
 }
--- Assets/AI/Actions/RainPickTarget.cs
+++ Assets/AI/Actions/RainPickTarget.cs
@@ -13,14 +13,17 @@ public class RainPickTarget : ActionBase
 		float rnd = Random.value;
 
 		// The robber will sometimes pick a random shop/bank, but will usually pick the nearest shop/bank
-		if (rnd < 0.2)
+		if (rnd < 0.5)
 		{
+			Debug.Log("Random Shop");
 			character.target = City.GetRandom (City.shops).GetComponent<Building>();
 		}
-		else if( rnd < 0.4 )
+		else //if( rnd < 0.4 )
 		{
+			Debug.Log ("Random Bank");
 			character.target = City.GetRandom (City.banks).GetComponent<Building>();
 		}
+		/*
 		else
 		{
 
@@ -31,14 +34,16 @@ public class RainPickTarget : ActionBase
 			if ((closestBank.transform.position - character.transform.position).sqrMagnitude 
 			    < (closestStore.transform.position - character.transform.position).sqrMagnitude) 
 			{
+				Debug.Log("Closest Bank");
 				character.target = closestBank.GetComponent<Building>();
 			}
 			else 
 			{
+				Debug.Log("Closest Store");
 				character.target = closestStore.GetComponent<Building>();
 			}
 		}
-
+		*/
         return ActionResult.SUCCESS;
     }
 }
