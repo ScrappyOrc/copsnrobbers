cc4a5fba652ef14975febba4a8d13740234209d8
Steven Sucy <sms6462@g.rit.edu>
2015-05-09 12:49:48 -0400

    Converted example program to the project

---
+++ Assets/Scripts/Bayes2.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class Bayes2 : MonoBehaviour 
+{
+	public struct Observation 
+	{
+		public int escapeIndex;
+		public int citizens;
+		public int cops;
+		public bool rob;
+	}
+
+	// Calculate the constant once
+	private readonly double sqrt2PI = Math.Sqrt(2.0 * Math.PI);
+	
+	// List of observations.  Initialized from the data file
+	// Added to with new observations during program run
+	private List<Observation> obsTab = new List<Observation> ();
+
+	// Value tables
+	int[,] escapeCondition;
+	double[,] escapeProportion;
+
+	int[] citizenSum = new int[2];
+	double[] citizenMean = new double[2];
+	double[] citizenStdDev = new double[2];
+	int[] citizenSumSq = new int[2];
+
+	int[] copSum = new int[2];
+	double[] copMean = new double[2];
+	double[] copStdDev = new double[2];
+	int[] copSumSq = new int[2];
+
+	int[] robCondition = new int[2];
+	double robProportion = new double[2];
+
+	public void Add(int escapeIndex, int citizens, int cops, bool rob)
+	{
+		Observation obs;
+		obs.escapeIndex = escapeIndex;
+		obs.citizens = citizens;
+		obs.cops = cops;
+		obs.rob = rob;
+		obsTab.Add (obs);
+	}
+
+	public void Build() 
+	{
+		foreach (Observation obs in obsTab) 
+		{
+			int robOff = obs.rob ? 0 : 1;
+
+			escapeCondition[(int)obs.escapeIndex, robOff]++;
+
+			citizenSum[robOff] += obs.citizens;
+			citizenSumSq[robOff] += obs.citizens * obs.citizens;
+
+			copSum[robOff] += obs.cops;
+			copSumSq[robOff] += obs.cops * obs.cops;
+
+			robCondition[robOff]++;
+		}
+
+		CalcProps (escapeCondition, robCondition, escapeProportion);
+
+		citizenMean [0] = Mean (citizenSum [0], robCondition [0]);
+		citizenMean [1] = Mean (citizenSum [1], robCondition [1]);
+		citizenStdDev [0] = StdDev (citizenSumSq [0], citizenSum [0], robCondition [0]);
+		citizenStdDev [1] = StdDev (citizenSumSq [1], citizenSum [1], robCondition [1]);
+
+		copMean [0] = Mean (copSum [0], robCondition [0]);
+		copMean [1] = Mean (copSum [1], robCondition [1]);
+		copStdDev [0] = StdDev (copSumSq [0], copSum [0], robCondition [0]);
+		copStdDev [1] = StdDev (copSumSq [1], copSum [1], robCondition [1]);
+
+		robProportion [0] = (double)robCondition [0] / obsTab.Count;
+		robProportion [1] = (double)robCondition [1] / obsTab.Count;
+	}
+
+	/******************************************************/
+	// Standard statistical functions.  These should be useful without modification.
+	
+	// Calculates the proportions for a discrete table of counts
+	// Handles the 0-frequency problem by assigning an artificially
+	// low value that is still greater than 0.
+	public void CalcProps (int[,] counts, int[] n, double[,] props)
+	{
+		for (int i = 0; i < counts.GetLength(0); i++)
+			for (int j = 0; j < counts.GetLength(1); j++)
+				// Detects and corrects a 0 count by assigning a proportion
+				// that is 1/10 the size of a proportion for a count of 1
+				if (counts[i,j] == 0)
+					props[i,j] = 0.1d / robCondition[j];	// Can't have 0
+		else
+			props[i,j] = (double)counts[i,j] / n[j];
+	}
+	
+	public double Mean (int sum, int n)
+	{
+		return (double)sum / n;
+	}
+	
+	public double StdDev(int sumSq, int sum, int n)
+	{
+		return Math.Sqrt((sumSq - (sum*sum)/(double)n) / (n-1));
+	}
+	
+	// Calculates probability of x in a normal distribution of
+	// mean and stdDev.  This corrects a mistake in the pseudo-code,
+	// used a power function instead of an exponential.
+	public double GauProb (double mean, double stdDev, int x)
+	{
+		double xMinusMean = x - mean;
+		return (1.0d / (stdDev*sqrt2PI)) * 
+			Math.Exp(-1.0d*xMinusMean*xMinusMean / (2.0d*stdDev*stdDev));
+	}
+	
+	/*********************************************************/
+
+	// Bayes likelihood for four condition values and one action value
+	// For each possible action value, call this with a specific set of four
+	// condition values, and pick the action that returns the highest
+	// likelihood as the most likely action to take, given the conditions.
+	public double CalcBayes(int escapeIndex, int citizens, int cops, bool rob)
+	{
+		int playOff = rob ? 0 : 1;
+		double like = escapeProportion[escapeIndex, playOff] *
+			GauProb(citizenMean[playOff], citizenStdDev[playOff], citizens) *
+				GauProb(copMean[playOff], copStdDev[playOff], cops) *
+				robProportion[playOff];
+		return like;
+	}
+}
